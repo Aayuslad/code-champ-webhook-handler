b8b02b8a5f4635875dd21fee46f528ec1aa09f1a
aayush <aayushlad058@gmail.com>
2024-09-12 10:40:00 +0530

    actual db call added

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,33 @@
 import express from "express";
 import cors from "cors";
+import { PrismaClient } from "@prisma/client";
+const Prisma = new PrismaClient();
 const app = express();
 const PORT = 5757;
 
 app.use(express.json());
-app.use(cors({
-	origin: ["*", "http://localhost:3001"],
-}));
+app.use(
+	cors({
+		origin: ["*", "http://localhost:3001"],
+	}),
+);
 
 app.get("/", (req, res) => {
 	res.json({ message: "Webhook handler is up and running! 🚀" });
 });
 
-app.post("/submit-task-callback", (req, res) => {
+app.post("/submit-task-callback", async (req, res) => {
 	console.log("Webhook received");
 	console.log(req.body);
+
+	await Prisma.submission.update({
+		where: {
+			id: req.body.submissionId,
+		},
+		data: {
+			status: req.body.accepted,
+		},
+	});
 
 	res.json({ message: "Webhook received" });
 });
